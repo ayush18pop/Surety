2e19fdf825b724df675d7d3ea8023129cec06bb3
ayush18pop <ayushsp05@gmail.com>
2026-07-08 13:03:59 +0530

    refactor block processing and transaction extraction

--- main.go
+++ main.go
@@ -14,6 +14,48 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func processBlock(client *ethclient.Client, ctx context.Context, blockNum uint64) error {
+	b, err := client.BlockByNumber(ctx, big.NewInt(int64(blockNum)))
+	if err != nil {
+		return err
+	}
+	fmt.Println("Block number:", b.Number())
+	fmt.Println("Block hash:", b.Hash())
+	fmt.Println("Timestamp:", b.Time())
+	fmt.Println("Transaction ", len(b.Transactions()))
+	for i, tx := range b.Transactions() {
+		fmt.Println("Index:", i)
+		if tx.Protected() {
+			signer := types.LatestSignerForChainID(tx.ChainId())
+			from, err := types.Sender(signer, tx)
+			if err != nil {
+				return err
+			}
+			fmt.Println("tx is form : ", from)
+			fmt.Println("chainid:", tx.ChainId())
+		} else {
+			signer := types.HomesteadSigner{}
+			from, err := types.Sender(signer, tx)
+			if err != nil {
+				return err
+			}
+			fmt.Println("tx is form : ", from)
+			fmt.Println("chainid:", tx.ChainId())
+
+		}
+		fmt.Println("Nonce:", tx.Nonce())
+		fmt.Println("To:", tx.To())
+		fmt.Println("Value:", tx.Value())
+		fmt.Println("Gas:", tx.Gas())
+		fmt.Println("Gas price:", tx.GasPrice())
+		fmt.Println("Data:", tx.Data())
+	}
+	fmt.Println("transactions : ")
+	fmt.Println("Gas used:", b.GasUsed())
+	fmt.Println("Gas limit:", b.GasLimit())
+	return nil
+}
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal(err)
@@ -22,9 +64,11 @@ func main() {
 	rpcURL := os.Getenv("ETH_MAINNET_RPC")
 
 	client, err := ethclient.Dial(rpcURL)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer client.Close()
 
 	ctx := context.Background()
 	checkpt, err := os.ReadFile("checkpoint.txt")
@@ -51,44 +95,10 @@ func main() {
 			fmt.Printf("Need to process blocks %d to %d\n", lastProcessed+1, latestNum)
 			for blockNum := lastProcessed + 1; blockNum <= latestNum; blockNum++ {
 				fmt.Println("Processing block:", blockNum)
-				b, err := client.BlockByNumber(ctx, big.NewInt(int64(blockNum)))
+				err = processBlock(client, ctx, blockNum)
 				if err != nil {
 					log.Fatal(err)
 				}
-				fmt.Println("Block number:", b.Number())
-				fmt.Println("Block hash:", b.Hash())
-				fmt.Println("Timestamp:", b.Time())
-				fmt.Println("Transaction ", len(b.Transactions()))
-				for i, tx := range b.Transactions() {
-					fmt.Println("Index:", i)
-					if tx.Protected() {
-						signer := types.LatestSignerForChainID(tx.ChainId())
-						from, err := types.Sender(signer, tx)
-						if err != nil {
-							log.Fatal(err)
-						}
-						fmt.Println("tx is form : ", from)
-						fmt.Println("chainid:", tx.ChainId())
-					} else {
-						signer := types.HomesteadSigner{}
-						from, err := types.Sender(signer, tx)
-						if err != nil {
-							log.Fatal(err)
-						}
-						fmt.Println("tx is form : ", from)
-						fmt.Println("chainid:", tx.ChainId())
-
-					}
-					fmt.Println("Nonce:", tx.Nonce())
-					fmt.Println("To:", tx.To())
-					fmt.Println("Value:", tx.Value())
-					fmt.Println("Gas:", tx.Gas())
-					fmt.Println("Gas price:", tx.GasPrice())
-					fmt.Println("Data:", tx.Data())
-				}
-				fmt.Println("transactions : ")
-				fmt.Println("Gas used:", b.GasUsed())
-				fmt.Println("Gas limit:", b.GasLimit())
 				lastProcessed = blockNum
 				data := []byte(strconv.FormatUint(lastProcessed, 10))
 				fileWriteErr := os.WriteFile("checkpoint.txt", data, 0666)
@@ -101,5 +111,4 @@ func main() {
 
 	}
 
-	// defer client.Close()
 }
